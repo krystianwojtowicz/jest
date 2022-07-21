121ababfd4c8db9c6c91be99937680df8294db42
Krystian <krystian1wojtowicz@gmail.com>
2022-07-21 06:37:49 +0200

    after 3 tests

--- reverseString.test.js
+++ reverseString.test.js
@@ -1,5 +1,17 @@
 const reverseString = require("./reverseString");
 
+// test('"what about spaces" becomes "secaps tuoba tahw"', () => {
+//   expect(reverseString("what about spaces")).toBe("secaps tuoba tahw");
+// });
+
+// test('"12345" becomes "54321"', () => {
+//   expect(reverseString("12345")).toBe("54321");
+// });
+
+// test('12345 becomes "not a string"', () => {
+//   expect(reverseString(12345)).toBe("not a string");
+// });
+
 test('"test" should become "tset"', () => {
   expect(reverseString("test")).toBe("tset");
 });
